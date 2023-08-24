a2f2b9240966ede94aab1467cd3657c9d8fe1a57
vaishnavi <vaishumurugaiah@gmail.com>
2023-08-24 19:08:40 +0530

    initial commit done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Weapon gunOne = new Weapon("gunOne", 30);
+        Weapon gunTwo = new Weapon("gunTwo", 50);
+
+        Player hero = new Player("src.Hero", gunOne);
+        Player villain = new Player("src.Villain", gunTwo);
+
+        System.out.println("The battle begins!");
+
+        villain.equipArmor();
+
+        int heroDamage = hero.weapon.getDamage();
+        villain.takeDamage(heroDamage);
+
+        int villainDamage = villain.weapon.getDamage();
+        hero.takeDamage(villainDamage);
+
+        System.out.println("The battle is over!");
+    }
+}
+
 class Weapon {
-    private String name;
-    private int damage;
+    String name;
+    int damage;
 
     public Weapon(String name, int damage) {
         this.name = name;
@@ -10,76 +32,32 @@ class Weapon {
     public int getDamage() {
         return damage;
     }
-
-    @Override
-    public String toString() {
-        return name;
-    }
 }
 
 class Player {
-    private String name;
-    private int health;
-    private Weapon weapon;
+    String name;
+    Weapon weapon;
+    boolean hasArmor;
 
-    public Player(String name, int health, Weapon weapon) {
+    public Player(String name, Weapon weapon) {
         this.name = name;
-        this.health = health;
         this.weapon = weapon;
+        this.hasArmor = false;
+    }
+
+    public void equipArmor() {
+        this.hasArmor = true;
     }
 
     public void takeDamage(int damage) {
-        health -= damage;
-        if (health < 0) {
-            health = 0;
+        if (hasArmor) {
+            System.out.println(name + " is wearing armor, so the damage is reduced.");
+            damage -= 10;
+            if (damage < 0) {
+                damage = 0;
+            }
         }
-    }
 
-    public Weapon getWeapon() {
-        return weapon;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Weapon gunOne = new Weapon("Gun One", 30);
-        Weapon gunTwo = new Weapon("Gun Two", 50);
-
-        Player hero = new Player("Hero", 100, gunOne);
-        Player villain = new Player("Villain", 100, gunTwo);
-
-        System.out.println("The battle begins: " + hero + " vs " + villain);
-        System.out.println(hero + " has chosen " + hero.getWeapon());
-        System.out.println(villain + " has chosen " + villain.getWeapon());
-
-        int heroDamage = hero.getWeapon().getDamage();
-        int villainDamage = villain.getWeapon().getDamage() - 10;
-
-        hero.takeDamage(villainDamage);
-        villain.takeDamage(heroDamage);
-
-        System.out.println(hero + " takes " + villainDamage + " damage.");
-        System.out.println(villain + " takes " + heroDamage + " damage.");
-
-        System.out.println(hero + " health: " + hero.getHealth());
-        System.out.println(villain + " health: " + villain.getHealth());
-
-        System.out.println("Battle result:");
-        if (hero.getHealth() > villain.getHealth()) {
-            System.out.println(hero + " wins!");
-        } else if (villain.getHealth() > hero.getHealth()) {
-            System.out.println(villain + " wins!");
-        } else {
-            System.out.println("It's a draw!");
-        }
+        System.out.println(name + " takes " + damage + " damage.");
     }
 }
